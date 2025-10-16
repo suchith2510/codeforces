ebe52a063436c14610d1a0d0aecdea1fc727dec4
suchThat <suchithpuppala@gmail.com>
2025-10-16 07:43:17 +0530

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.java
@@ -0,0 +1,1 @@
+import java.util.*;public class Main {    public static void main(String[] args) {        Scanner sc=new Scanner(System.in);        int x=sc.nextInt();        int sol=x/5;        if(x%5!=0) sol++;        System.out.println(sol);    }}
