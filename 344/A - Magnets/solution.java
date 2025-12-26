27449ee34740aba27c60e6aebcc4810043089936
suchThat <suchithpuppala@gmail.com>
2025-12-26 14:11:38 +0530

    Add Magnets [A] from Codeforces

---
+++ 344/A - Magnets/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner;public class Main {    public static void main(String[] args) {        Scanner s = new Scanner(System.in);        int n=s.nextInt();        String a=s.next();        int c=1;        for (int i=1;i<n;i++) {            String b=s.next();            if (!b.equals(a))                c++;            a = b;        }        System.out.print(c);    }}
