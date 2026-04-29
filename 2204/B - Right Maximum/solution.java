ee580521254b1e22b9230785e9a23146854ea6ad
suchThat <suchithpuppala@gmail.com>
2026-04-29 10:13:15 +0530

    Add Right Maximum [B] from Codeforces

---
+++ 2204/B - Right Maximum/solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            int max=-10000;
+            int count=0;
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+                max=Math.max(max,arr[i]);
+                if(arr[i]==max) count++;
+            }
+        System.out.println(count);
+        }
+}
+}
